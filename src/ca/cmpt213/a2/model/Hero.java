dd0589e3be0685dd81c387bba27b0e3dd7b3e03e
kashk94 <47699876+kashk94@users.noreply.github.com>
2020-06-22 19:27:32 -0700

    Added .gitignore to ignore 'out' directory

---
+++ src/ca/cmpt213/a2/model/Hero.java
@@ -0,0 +1,52 @@
+package ca.cmpt213.a2.model;
+
+public class Hero {
+    private static int powerCount = 0;
+    private Position heroPosition = new Position(1, 1);
+    private boolean isAlive = true;
+
+
+    public static int getPowerCount() {
+        return powerCount;
+    }
+
+    public void incrementPowerCount() {
+        powerCount++;
+    }
+
+    public void decrementPowerCount() {
+        powerCount--;
+    }
+
+    public void moveHeroLeft() {
+        heroPosition.moveLeft();
+    }
+
+    public void moveHeroRight() {
+        heroPosition.moveRight();
+    }
+
+    public void moveHeroUp() {
+        heroPosition.moveUp();
+    }
+
+    public void moveHeroDown() {
+        heroPosition.moveDown();
+    }
+
+    public int getHeroXPos() {
+        return heroPosition.getXPosition();
+    }
+
+    public int getHeroYPos() {
+        return heroPosition.getYPosition();
+    }
+
+    public boolean getHeroLifeStatus() {
+        return isAlive;
+    }
+
+    public void killHero() {
+        isAlive = false;
+    }
+} // Hero.java
---
+++ src/ca/cmpt213/a2/model/Power.java
@@ -0,0 +1,7 @@
+package ca.cmpt213.a2.model;
+
+public class Power {
+    public Power(int xPos, int yPos) {
+        Position powerPosition = new Position(xPos, yPos);
+    }
+} // Power.java
