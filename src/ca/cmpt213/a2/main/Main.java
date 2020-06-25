c69a5f5e2948d51fac54ef2a44d3b191778a5cfc
kashk94 <47699876+kashk94@users.noreply.github.com>
2020-06-25 00:25:45 -0700

    Minor styling and syntax changes

--- src/ca/cmpt213/a2/main/Main.java
+++ src/ca/cmpt213/a2/main/Main.java
@@ -2,23 +2,33 @@ package ca.cmpt213.a2.main;
 import  ca.cmpt213.a2.gameui.*;
 import ca.cmpt213.a2.model.*;
 
-import java.util.Random;
-
 public class Main {
+
+    private static final int FIRST_MONSTER_INITIAL_X_POS = 18;
+    private static final int FIRST_MONSTER_INITIAL_Y_POS = 1;
+    private static final int SECOND_MONSTER_INITIAL_X_POS = 1;
+    private static final int SECOND_MONSTER_INITIAL_Y_POS = 13;
+    private static final int THIRD_MONSTER_INITIAL_X_POS = 18;
+    private static final int THIRD_MONSTER_INITIAL_Y_POS = 13;
+
     public static void main(String []args) {
 
         TextUI.mainDisplay();
+
         Maze mazeMap = new Maze();
         Hero theHero = new Hero();
-        Monster monster1 = new Monster(18,1);
-        Monster monster2 = new Monster(1,13);
-        Monster monster3 = new Monster(18,13);
-        mazeMap.putHeroPosition(theHero);
-        mazeMap.putMonsterPosition(monster1);
-        mazeMap.putMonsterPosition(monster2);
-        mazeMap.putMonsterPosition(monster3);
-        mazeMap.putPowerRandomly();
+        Monster monster1 = new Monster(FIRST_MONSTER_INITIAL_X_POS, FIRST_MONSTER_INITIAL_Y_POS);
+        Monster monster2 = new Monster(SECOND_MONSTER_INITIAL_X_POS, SECOND_MONSTER_INITIAL_Y_POS);
+        Monster monster3 = new Monster(THIRD_MONSTER_INITIAL_X_POS, THIRD_MONSTER_INITIAL_Y_POS);
+
+        mazeMap.setHeroPositionInMaze(theHero);
+        mazeMap.setMonsterPositionInMaze(monster1);
+        mazeMap.setMonsterPositionInMaze(monster2);
+        mazeMap.setMonsterPositionInMaze(monster3);
+        mazeMap.placePowerRandomlyInMaze();
         mazeMap.displayCurrMaze();
+
+
 
 //        int monsters_kill = TextUI.inputMonstersKill();
 //        int powers = TextUI.inputPowers();
